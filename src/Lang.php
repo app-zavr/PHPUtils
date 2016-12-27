bae259a9297bfb28261c0ee7b62028390223c355
Tema Snow <tema.slizhik@gmail.com>
2016-12-28 02:18:29 +0700

    fixed bug;

--- src/Lang.php
+++ src/Lang.php
@@ -15,7 +15,7 @@ function getBrowserLanguage($default = 'en', $languages = ['ru', 'en'])
 {
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
-    if (array_key_exists($lang, $languages))
+    if (in_array($lang, $languages))
         return $lang;
     else
         return $default;
